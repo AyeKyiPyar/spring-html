82ac901488c28ce1770513cc19cfb5fc6a99879f
AyeKyiPyar <49303336+AyeKyiPyar@users.noreply.github.com>
2025-10-28 14:01:37 +0630

    Update HelloController.java

--- src/main/java/com/akps/controller/HelloController.java
+++ src/main/java/com/akps/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController 
 {
-	@GetMapping("/index")
+	@GetMapping("/welcome")
 	public String index()
 	{
 		return "hello.html";
